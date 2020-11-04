a90b7745f032f50a1ada1fac461d84491051a4b8
Noah Bliss <noah@superuser.sh>
2020-11-04 13:18:15 -0500

    Create client.jl

---
+++ client.jl
@@ -0,0 +1,54 @@
+#!/usr/bin/env julia
+import HTTP
+import JSON
+using DelimitedFiles
+
+#settingsfile = "spike.conf"
+backupsettingsfile = "$(homedir())/.config/o365webhook-julia/spike.conf"
+@isdefined(settingsfile) || isfile(backupsettingsfile) && (settingsfile = backupsettingsfile)
+isfile(settingsfile) || println("Settings file is missing.") && exit(1)
+
+importedvars = readdlm(settingsfile, '=', String; skipblanks=true)
+a2var(key, a) = (c=1; for i in a[:, 1]; i == key && return a[c, 2]; c+=1; end || error("$key not found"))
+
+uri = a2var("uri", importedvars)
+
+function webreq(uri, payload)
+        #uri = "$uri/api/$query"
+        headers = ["Content-Type" => "application/json" ]
+        payload = JSON.json(payload)
+        response = HTTP.request("POST", uri, headers, payload; require_ssl_verification = true)
+        #return String(response.body)
+        if response.status == 200
+                return JSON.parse(String(response.body))
+        else
+                error(response.status)
+        end
+        # return response.body
+end
+
+
+
+title = "Title"
+message = "This is the message body."
+buttontext = "Click me!"
+buttonuri = "https://duck.com"
+
+payload = Dict(
+        "@context" => "https://schema.org/extensions",
+        "@type" => "MessageCard",
+        "themeColor" => "383838",
+        "title" => title,
+        "text" => message,
+        "potentialAction" => Any[Dict(
+                "@type" => "OpenUri",
+                "name" => buttontext,
+                "targets" => Any[Dict(
+                        "os" => "default",
+                        "uri" => buttonuri
+                        )]
+                )]
+)
+
+response = webreq(uri, payload)
+
